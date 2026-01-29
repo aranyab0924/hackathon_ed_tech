bec592bd2c3a4951e91bf13c4ccf69aec89f6f3f
Tvick22 <142449252+Tvick22@users.noreply.github.com>
2026-01-29 11:01:07 -0800

    initial commit: init.sh file

---
+++ scripts/init.sh
@@ -0,0 +1,13 @@
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip install --upgrade pip
+
+pip install -r requirements.txt
+
+gem install bundler -v 2.6.9
+
+bundle _2.6.9_ install
+
+bundle _2.6.9_ exec jekyll build

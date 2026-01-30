201e2512c9dd29855588eeae951f341465df0483
Tvick22 <142449252+Tvick22@users.noreply.github.com>
2026-01-30 10:21:35 -0800

    fix: gem path

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,7 +6,12 @@ pip install --upgrade pip
 
 pip install -r requirements.txt
 
+sudo mkdir -p "$GEM_HOME"
+sudo chmod -R 777 "$GEM_HOME"
+
 gem install bundler -v 2.6.9
+
+gem install jekyll
 
 bundle _2.6.9_ install
 
